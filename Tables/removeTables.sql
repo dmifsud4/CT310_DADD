29d859a148f3f861c14cc65e0f33bd721a8d42a2
David <dsant8@rams.colostate.edu>
2019-03-31 20:41:27 -0600

    Added script to remove tables for dev

---
+++ Tables/removeTables.sql
@@ -0,0 +1,9 @@
+drop table if exists baseHospital;
+drop table if exists baseEfficiency;
+drop table if exists baseHCAHPS;
+drop table if exists baseCare;
+
+drop table if exists scenarioHospital;
+drop table if exists scenarioEfficiency;
+drop table if exists scenarioHCAHPS;
+drop table if exists scenarioCare;
